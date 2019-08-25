d645266f78cfc847eeb3638717ed58c8815fcc25
yueyi <yzha0168@student.monash.edu>
2019-08-26 07:52:49 +1000

    Add logo

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">Your Guider</a>
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="img/icon.JPG" style="margin-right:.3rem;margin-bottom:3px;width:1.2rem;height:1.2rem;">Your Guider</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
