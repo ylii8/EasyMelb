f295013d9c4a3950160a8ca60e95c849355625c5
yueyi <yzha0168@student.monash.edu>
2019-08-27 18:22:40 +1000

    map filter functions

--- locations_model.php
+++ locations_model.php
@@ -1,25 +1,79 @@
 <?php
 
-// Gets data from URL parameters.
-function get_all_locations(){
-    $con=mysqli_connect ("127.0.0.1", 'root', 'root','light');
-    if (!$con) {
-        die('Not connected : ' . mysqli_connect_error());
+    function connect_db()
+    {
+        $con=mysqli_connect ("127.0.0.1", 'root', 'root','light');
+        if (!$con) {
+            die('Not connected : ' . mysqli_connect_error());
+        }
+        return $con;
     }
-    // update location with location_status if admin location_status.
-    $sqldata = mysqli_query($con,"select sensor_id ,sensor_name,latitude,longitude,sensor_description,status from sensor_locations");
 
-    $rows = array();
-    while($r = mysqli_fetch_assoc($sqldata)) {
-        $rows[] = $r;
-    }
-  $indexed = array_map('array_values', $rows);
-  //  $array = array_filter($indexed);
+    // Gets data from URL parameters.
+    function get_seats_locations()
+    {
+        $con= connect_db();
+        // update location with location_status if admin location_status.
 
-    echo json_encode($indexed);
-    if (!$rows) {
-        return null;
+        $seatData = mysqli_query($con,"select LOCATION_DESC,lat,lon from seats");
+
+        $rows = array();
+        while($r = mysqli_fetch_assoc($seatData)) {
+            $rows[] = $r;
+        }
+
+      $indexed = array_map('array_values', $rows);
+      //  $array = array_filter($indexed);
+
+        echo json_encode($indexed);
+        if (!$rows) {return null;}
     }
-}
+
+    function get_drink_locations()
+    {
+        $con= connect_db();
+        // update location with location_status if admin location_status.
+
+        $drinkData = mysqli_query($con,"select Description,lat,lon from drinking_fountains");
+
+        $rows = array();
+        while($r = mysqli_fetch_assoc($drinkData)) {
+            $rows[] = $r;
+        }
+
+        $indexed = array_map('array_values', $rows);
+        //  $array = array_filter($indexed);
+
+        echo json_encode($indexed);
+        if (!$rows) {return null;}
+    }
+
+    function get_toilet_locations()
+    {
+        $con= connect_db();
+        // update location with location_status if admin location_status.
+
+        $toiletData = mysqli_query($con,"select name,lat,lon from toilets");
+
+        $rows = array();
+        while($r = mysqli_fetch_assoc($toiletData)) {
+            $rows[] = $r;
+        }
+        $indexed = array_map('array_values', $rows);
+        //  $array = array_filter($indexed);
+
+        echo json_encode($indexed);
+        if (!$rows) {return null;}
+    }
+
+//    function get_sqlData()
+//    {
+//        $con= connect_db();
+//        if
+//        $seatData = mysqli_query($con,"select LOCATION_DESC,lat,lon from seats");
+//        $sqlData = mysqli_query($con,"select Description,lat,lon from drinking_fountains");
+//        $sqlData = mysqli_query($con,"select name,lat,lon from toilets");
+//        return $sqlData;
+//    }
 
 ?>
--- map.php
+++ map.php
@@ -3,36 +3,72 @@
 <head>
     <?php include 'header.php'; ?>
 
-    <script type="text/javascript" src="js/googleMap.js"></script>
     <style>
         /* Always set the map height explicitly to define the size of the div
          * element that contains the map. */
         #map {
-            margin-top: 3rem;
-            height: 600px;
+            height: 100%;
             width: 100%;
+            border: 1px solid lightyellow;
             background-color:grey;
+            shape-outside: ellipse();
         }
-        #lightData{
-            display:none;
+        .container_map{
+            height:600px;
         }
+        .filter-box {
+            padding: 5px 10px;
+            border: 1px solid #e8e8e8;
+        }
+        .marker-filter hr{
+            width:0;
+        }
+
     </style>
 </head>
 
 <body>
 
 <!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+<nav class="navbar navbar-expand-lg navbar-dark sticky-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="index.php"><img src="img/logo.PNG" style="margin-right:.3rem;margin-bottom:3px;width:2.2rem;height:2.2rem;">Your Guider</a>
+        <a class="navbar-brand js-scroll-trigger" href="index.html"><img src="img/logo.PNG" style="margin-right:.3rem;margin-bottom:3px;width:2.2rem;height:2.2rem;">Your Guider</a>
     </div>
 </nav>
 
 
+
 <!--map-->
 
-<div class="container">
-    <div id="map"></div>
+<div class="container_map row no-gutters">
+    <!-- Filter Checkboxes -->
+    <div class="marker-filter col-lg-1">
+        <hr>
+        <span class="filter-box">
+            <label for="seats">
+                <input type="checkbox" name="seats" id="seats" onclick="getSeats()">
+                Seats
+            </label>
+        </span>
+        <hr>
+        <span class="filter-box">
+            <label for="toilets">
+                <input type="checkbox" name="toilets"  id="toilets" onclick="getToilet()">
+                Toilets
+            </label>
+        </span>
+        <hr>
+        <span class="filter-box">
+            <label for="drinking_fountains">
+                <input type="checkbox" name="drinking_fountains" id="drinking_fountains" onclick="getDrink()">
+                Drinking Fountains
+            </label>
+        </span>
+        <p id="seatText" style="display:none">Seats is SELECTED!</p>
+        <p id="drinkText" style="display:none">Drinking Fountains is SELECTED!</p>
+        <p id="toiletText" style="display:none">Toilets is SELECTED!</p>
+    </div>
+    <div class="col-lg-11" id="map"></div>
 </div>
 
 
@@ -59,44 +95,121 @@
 <!-- GoogleMap js (show markers and click events)-->
 <?php include_once 'locations_model.php'; ?>
 <script>
-var map;
-var marker;
-var infowindow;
-var red_icon =  'http://maps.google.com/mapfiles/ms/icons/red-dot.png' ;
-var purple_icon =  'http://maps.google.com/mapfiles/ms/icons/purple-dot.png' ;
-var locations = <?php get_all_locations() ?>;
 
-function initMap() {
-var Melbourne = {lat: -37.8136, lng: 144.9631};
-infowindow = new google.maps.InfoWindow();
-map = new google.maps.Map(document.getElementById('map'), {
-center: Melbourne,
-zoom: 15
-});
+    var map;
+    var marker;
+    var markers =[];
+    var infowindow;
+    var red_icon =  'http://maps.google.com/mapfiles/ms/icons/red-dot.png' ;
+    var purple_icon =  'http://maps.google.com/mapfiles/ms/icons/purple-dot.png' ;
+    var locations;
 
-var i ;
-for (i = 0; i < locations.length; i++) {
+    function getSeats(){
 
-    marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][2], locations[i][3]),
-        map: map,
-        icon: locations[i][5] === 'A' ? red_icon : purple_icon,
-        html: document.getElementById('form')
-
-    });
-
-    google.maps.event.addListener(marker, 'click', (function (marker, i) {
-        return function () {
-            $("#id").val(locations[i][0]);
-            $("#description").val(locations[i][4]);
-            $("#form").show();
-            infowindow.setContent(marker.html);
-            infowindow.open(map, marker);
+        var checkBox = document.getElementById("seats");
+        var text = document.getElementById("seatText");
+        if (checkBox.checked == true)
+        {
+            text.style.display = "block";
+            locations = <?php get_seats_locations() ?>;
+            addMarkers();
         }
-    })(marker, i));
+        else
+        {
+            text.style.display = "none";
+            removeMarker();
+        }
+    }
+
+    function getDrink(){
+
+        var checkBox = document.getElementById("drinking_fountains");
+        var text = document.getElementById("drinkText");
+        if (checkBox.checked == true)
+        {
+            text.style.display = "block";
+            locations = <?php get_drink_locations() ?>;
+            addMarkers();
+        }
+        else
+        {
+            text.style.display = "none";
+            removeMarker();
+        }
+    }
+
+    function getToilet(){
+
+        var checkBox = document.getElementById("toilets");
+        var text = document.getElementById("toiletText");
+        if (checkBox.checked == true)
+        {
+            text.style.display = "block";
+            locations = <?php get_toilet_locations() ?>;
+            addMarkers();
+        }
+        else
+        {
+            text.style.display = "none";
+            removeMarker();
+        }
+    }
+
+
+    function initMap()
+    {
+
+        var Melbourne = {lat: -37.8136, lng: 144.9631};
+        infowindow = new google.maps.InfoWindow();
+        map = new google.maps.Map(document.getElementById('map'), {
+        center: Melbourne,
+        zoom: 15
+        });
+        // addMarkers();
 
     }
-}
+
+    // check checkbox first, get data, add marker
+
+
+    function addMarkers()
+    {
+        var i ;
+        for (i = 0; i < locations.length; i++)
+        {
+
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                map: map,
+                // icon: locations[i][5] === 'A' ? red_icon : purple_icon,
+                html: document.getElementById('form')
+
+            });
+
+            google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                return function () {
+                    // $("#id").val(locations[i][0]);
+                    $("#description").val(locations[i][4]);
+                    $("#form").show();
+                    infowindow.setContent(marker.html);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+            markers.push(marker);
+
+        }
+    }
+
+    function removeSeatsMarker()
+    {
+
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(null);
+        }
+    }
+
+
+
 </script>
 
 
