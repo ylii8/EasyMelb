3104d8f5a5ae99582a2f69cc0cf6b286319ec3ef
yueyi <yzha0168@student.monash.edu>
2019-08-28 17:25:41 +1000

    Add about us

---
+++ aboutUs.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'header.php'; ?>
+</head>
+
+<body>
+
+
+<section class="yellow bg-primary" id="features">
+    <div class="container">
+        <div class="section-heading text-center">
+            <h2>About us</h2>
+            <hr id="hrstyle" style="width: 50%; margin-top: 40px;">
+        </div>
+    </div>
+</section>
+
+<section class="features" style="padding-top:50px;padding-bottom:50px;">
+    <div class="container">
+        <div class="section-heading text-center">
+            <div class="row">
+                <div class="col-lg-12 my-auto">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="feature-item">
+                                    <div class="card">
+                                        <img class="card-imd-top" style="height:420px;width:325px;" src="img/yueyi.jpg">
+                                        <div class="card-body">
+                                            <h4 class="card-title">Yueyi Zhao</h4>
+                                            <p class="card-text">Yueyi Zhao is from Master of Information Technology.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="feature-item">
+                                    <div class="card">
+                                        <img class="card-imd-top" style="height:420px;width:325px; src="img/yu.jpg">
+                                        <div class="card-body">
+                                            <h4 class="card-title">Yu Li</h4>
+                                            <p class="card-text">Yu Li is from Master of Data Science.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="feature-item">
+                                    <div class="card">
+                                        <img class="card-imd-top" style="height:420px;width:325px;" src="img/cui.jpg">
+                                        <div class="card-body">
+                                            <h4 class="card-title">Lu Cui</h4>
+                                            <p class="card-text">Lu Cui is from Master of Business Information System.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="feature-item">
+                                    <div class="card">
+                                        <img class="card-imd-top" style="height:420px;width:325px; src="img/binh.jpg">
+                                        <div class="card-body">
+                                            <h4 class="card-title">Thuy Tri Binh Tran</h4>
+                                            <p class="card-text">Binh is from Master of Business Information System.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+<footer>
+    <?php include 'footer.php'; ?>
+</footer>
+</body>
+
+
+</html>
